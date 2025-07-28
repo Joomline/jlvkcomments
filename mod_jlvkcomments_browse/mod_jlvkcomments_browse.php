9c19b45efad6965945b4176f3debf41ce6c68b06
Vadim Kunitsyn <zikkuratvk@gmail.com>
2025-07-28 16:42:30 +0200

    модуль ленты комментариев вконтакте

---
+++ mod_jlvkcomments_browse/mod_jlvkcomments_browse.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * JoomLine VK Comments Browse Module
+ *
+ * @version 1.0.0
+ * @author Anton Voynov (anton@joomline.ru)
+ * @copyright (C) 2010-2024 by Anton Voynov(http://www.joomline.ru)
+ * @license GNU/GPL: http://www.gnu.org/copyleft/gpl.html
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Helper\ModuleHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\WebAsset\WebAssetManager;
+
+// Include helper file
+require_once __DIR__ . '/helper.php';
+
+// Get module parameters
+$apiId = $params->get('apiId');
+$width = $params->get('width', 500);
+$height = $params->get('height', 0);
+$limit = $params->get('limit', 10);
+$mini = $params->get('mini', 'auto');
+$norealtime = $params->get('norealtime', 0);
+
+// Add VK API script using WebAssetManager
+$wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+$wa->registerAndUseScript('vk-api-browse', 'https://vk.com/js/api/openapi.js?169', [], []);
+
+// Generate widget HTML
+$widgetHtml = '<div id="vk_comments_browse"></div>';
+$widgetHtml .= '<script type="text/javascript">';
+$widgetHtml .= 'function initVKCommentsBrowse() {';
+$widgetHtml .= 'if (typeof VK !== "undefined") {';
+$widgetHtml .= 'VK.init({apiId: ' . (int)$apiId . ', onlyWidgets: true});';
+$widgetHtml .= 'VK.Widgets.CommentsBrowse("vk_comments_browse", {';
+$widgetHtml .= 'width: ' . (int)$width . ', ';
+if ($height > 0) {
+    $widgetHtml .= 'height: ' . (int)$height . ', ';
+}
+$widgetHtml .= 'limit: ' . (int)$limit . ', ';
+if ($mini !== 'auto') {
+    $widgetHtml .= 'mini: ' . (int)$mini . ', ';
+}
+$widgetHtml .= 'norealtime: ' . (int)$norealtime;
+$widgetHtml .= '});';
+$widgetHtml .= '} else {';
+$widgetHtml .= 'setTimeout(initVKCommentsBrowse, 500);';
+$widgetHtml .= '}';
+$widgetHtml .= '}';
+$widgetHtml .= 'document.addEventListener("DOMContentLoaded", initVKCommentsBrowse);';
+$widgetHtml .= '</script>';
+
+require ModuleHelper::getLayoutPath('mod_jlvkcomments_browse', 'default'); 
---
+++ mod_jlvkcomments_browse/tmpl/default.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * JoomLine VK Comments Browse Module - Default Layout
+ *
+ * @version 1.0.0
+ * @author Anton Voynov (anton@joomline.ru)
+ * @copyright (C) 2010-2024 by Anton Voynov(http://www.joomline.ru)
+ * @license GNU/GPL: http://www.gnu.org/copyleft/gpl.html
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+// Output widget HTML
+echo $widgetHtml; 

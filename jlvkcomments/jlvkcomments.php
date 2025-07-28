a8030bdf38456a96ab1ab3515084801de9baacf0
Vadim Kunitsyn <zikkuratvk@gmail.com>
2025-07-28 10:49:18 +0200

    Базовая совместимость с Joomla 5

--- jlvkcomments/jlvkcomments.php
+++ jlvkcomments/jlvkcomments.php
@@ -2,87 +2,132 @@
 /**
  * Jlvkcomments
  *
- * @version 2.0.0
+ * @version 3.0.0
  * @author Vadim Kunicin(vadim@joomline.ru), Anton Voynov (anton@joomline.ru)
- * @copyright (C) 2010-2022 by Anton Voynov(http://www.joomline.ru)
+ * @copyright (C) 2010-2024 by Anton Voynov(http://www.joomline.ru)
  * @license GNU/GPL: http://www.gnu.org/copyleft/gpl.html
  **/
 
 // no direct access
 defined('_JEXEC') or die;
 
-jimport('joomla.plugin.plugin');
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 
-
-class plgContentJlvkcomments extends JPlugin
+/**
+ * Jlvkcomments plugin
+ *
+ * @since  3.0.0
+ */
+class PlgContentJlvkcomments extends CMSPlugin
 {
+	/**
+	 * Load the language file on instantiation.
+	 *
+	 * @var    boolean
+	 * @since  3.0.0
+	 */
+	protected $autoloadLanguage = true;
 
-	public function onContentPrepare($context, &$article, &$params, $page = 0){
-	if($context == 'com_content.article'){
-
-		JPlugin::loadLanguage( 'plg_content_Jlvkcomments' );	
-
-
-
-		if (strpos($article->text, '{jlvkcomments-off}') !== false) {
-			$article->text = str_replace("{jlvkcomments-off}","",$article->text);
-			return true;
+	/**
+	 * Plugin that adds VK comments to articles
+	 *
+	 * @param   string   $context  The context of the content being passed to the plugin.
+	 * @param   object   &$article  The article object.  Note $article->text is also available
+	 * @param   object   &$params  The article params
+	 * @param   integer  $page     The 'page' number
+	 *
+	 * @return  void
+	 *
+	 * @since   3.0.0
+	 */
+	public function onContentPrepare($context, &$article, &$params, $page = 0)
+	{
+		if ($context !== 'com_content.article')
+		{
+			return;
 		}
 
-		if (strpos($article->text, '{jlvkcomments}') === false && !$this->params->def('autoAdd')) {
-			return true;
+		// Check if comments are disabled for this article
+		if (strpos($article->text, '{jlvkcomments-off}') !== false) 
+		{
+			$article->text = str_replace("{jlvkcomments-off}", "", $article->text);
+			return;
 		}
 
-		$exceptcat = is_array($this->params->def('categories')) ? $this->params->def('categories') : array($this->params->def('categories'));
-		if (!in_array($article->catid,$exceptcat)) {
-			$view = JFactory::getApplication()->input->get('view');
-			if ($view == 'article') {
-
-				$doc = &JFactory::getDocument();
-				
-				$apiId 			= $this->params->def('apiId');
-				$width 			= $this->params->def('width');
-				$comLimit 		= $this->params->def('comLimit');
-				$attach 		= $this->params->def('attach');
-				$autoPublish 	= $this->params->def('autoPublish');
-				$norealtime 	= $this->params->def('norealtime');
-				$script 		= "VK.init({apiId: $apiId, onlyWidgets: true});";				
-				$doc->addScript('//vk.com/js/api/openapi.js?169');
-				$doc->addScriptDeclaration($script);
-
-				$pagehash = $article->id;
-				$scriptPage .= <<<HTML
-				
-					<div id='jlvkcomments'></div>
-					<script type='text/javascript'>
-					VK.Widgets.Comments('jlvkcomments', {limit: $comLimit, width: '$width', attach: '$attach', autoPublish: $autoPublish, norealtime: $norealtime},$pagehash);
-					</script>
-					
-					
-				
-					
-HTML;
-				if (in_array($pagehash, array(5,16,31,47,74,99,145,288,447,897,1363,2461))){
-					$scriptPage .= <<<HTML
-					<div style="text-align: right;">
-						<a style="text-decoration:none; color: #c0c0c0; font-family: arial,helvetica,sans-serif; font-size: 5pt; " target="_blank" href="https://joomline.net">https://joomline.net</a>
-					</div>
-					
-HTML;
-				}
-				
-				if ($this->params->def('autoAdd') == 1) {
-					$article->text .= $scriptPage;
-				} else {
-					$article->text = str_replace("{jlvkcomments}",$scriptPage,$article->text);
-				}
-
-			}
-		} else {
-			$article->text = str_replace("{jlvkcomments}","",$article->text);
+		// Check if auto-add is disabled and no manual tag is present
+		if (strpos($article->text, '{jlvkcomments}') === false && !$this->params->get('autoAdd')) 
+		{
+			return;
 		}
 
+		// Check categories exclusion
+		$excludedCategories = $this->params->get('categories', array());
+		if (!is_array($excludedCategories)) 
+		{
+			$excludedCategories = array($excludedCategories);
+		}
+
+		if (in_array($article->catid, $excludedCategories)) 
+		{
+			$article->text = str_replace("{jlvkcomments}", "", $article->text);
+			return;
+		}
+
+		// Only add comments on article view
+		$app = Factory::getApplication();
+		$view = $app->input->get('view');
+		
+		if ($view !== 'article') 
+		{
+			return;
+		}
+
+		// Get plugin parameters
+		$apiId = $this->params->get('apiId');
+		$width = $this->params->get('width', 0);
+		$comLimit = $this->params->get('comLimit', 10);
+		$attach = $this->params->get('attach', '*');
+		$autoPublish = $this->params->get('autoPublish', 1);
+		$norealtime = $this->params->get('norealtime', 0);
+
+		// Add VK API script
+		$doc = Factory::getDocument();
+		$doc->addScript('//vk.com/js/api/openapi.js?169');
+		
+		$script = "VK.init({apiId: " . (int)$apiId . ", onlyWidgets: true});";
+		$doc->addScriptDeclaration($script);
+
+		// Generate comments widget HTML
+		$pagehash = $article->id;
+		$scriptPage = '<div id="jlvkcomments"></div>';
+		$scriptPage .= '<script type="text/javascript">';
+		$scriptPage .= 'VK.Widgets.Comments("jlvkcomments", {';
+		$scriptPage .= 'limit: ' . (int)$comLimit . ', ';
+		$scriptPage .= 'width: "' . (int)$width . '", ';
+		$scriptPage .= 'attach: "' . $attach . '", ';
+		$scriptPage .= 'autoPublish: ' . (int)$autoPublish . ', ';
+		$scriptPage .= 'norealtime: ' . (int)$norealtime;
+		$scriptPage .= '}, ' . (int)$pagehash . ');';
+		$scriptPage .= '</script>';
+
+		// Add attribution link (only on specific pages)
+		if (in_array($pagehash, array(5, 16, 31, 47, 74, 99, 145, 288, 447, 897, 1363, 2461)))
+		{
+			$scriptPage .= '<div style="text-align: right;">';
+			$scriptPage .= '<a style="text-decoration:none; color: #c0c0c0; font-family: arial,helvetica,sans-serif; font-size: 5pt;" target="_blank" href="https://joomline.net">https://joomline.net</a>';
+			$scriptPage .= '</div>';
+		}
+
+		// Add comments to article
+		if ($this->params->get('autoAdd')) 
+		{
+			$article->text .= $scriptPage;
+		} 
+		else 
+		{
+			$article->text = str_replace("{jlvkcomments}", $scriptPage, $article->text);
+		}
 	}
-}
-
 }
--- jlvkcommentson/jlvkcommentson.php
+++ jlvkcommentson/jlvkcommentson.php
@@ -1,25 +1,48 @@
 <?php
+/**
+ * Jlvkcommentson plugin
+ *
+ * @version 2.0.0
+ * @author Anton Voynov (anton@joomline.ru)
+ * @copyright (C) 2010-2024 by Anton Voynov(http://www.joomline.ru)
+ * @license GNU/GPL: http://www.gnu.org/copyleft/gpl.html
+ */
+
 // no direct access
 defined('_JEXEC') or die;
 
-jimport('joomla.plugin.plugin');
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Object\CMSObject;
 
-class plgButtonJlvkcommentson extends JPlugin
+/**
+ * Jlvkcommentson plugin
+ *
+ * @since  2.0.0
+ */
+class PlgButtonJlvkcommentson extends CMSPlugin
 {
-	public function __construct(& $subject, $config)
-	{
-		parent::__construct($subject, $config);
-		$this->loadLanguage();
-	}
+	/**
+	 * Load the language file on instantiation.
+	 *
+	 * @var    boolean
+	 * @since  2.0.0
+	 */
+	protected $autoloadLanguage = true;
 
+	/**
+	 * Display the button
+	 *
+	 * @param   string  $name  The name of the button to add
+	 *
+	 * @return  CMSObject  The button options as CMSObject
+	 *
+	 * @since   2.0.0
+	 */
 	public function onDisplay($name)
 	{
-		$app 		= JFactory::getApplication();
-
-		$doc		= JFactory::getDocument();
-		$template	= $app->getTemplate();
-
-
+		$app = Factory::getApplication();
+		$doc = Factory::getDocument();
 
 		$getContent = $this->_subject->getContent($name);
 
@@ -34,19 +57,15 @@ class plgButtonJlvkcommentson extends JPlugin
 			}
 		";
 
-
 		$doc->addScriptDeclaration($js);
 
-		$button = new JObject;
+		$button = new CMSObject;
 		$button->set('modal', false);
-		$button->set('onclick', 'insertJlvkcommentson(\''.$name.'\');return false;');
+		$button->set('onclick', 'insertJlvkcommentson(\'' . $name . '\');return false;');
 		$button->set('text', "Вкл JLVKComments");
 		$button->set('name', 'butttonjlvkcommentson');
-
-
 		$button->set('link', '#');
 
 		return $button;
-
 	}
 }
